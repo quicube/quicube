67f01e6b8990fc8b8e607249e1a5ff41a931deb3
quicube <quicubecoder@gmail.com>
2021-08-30 18:59:49 +0900

    content

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <div className="Header">
+            <h1>Qui Cube</h1>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/components/Main.tsx
@@ -0,0 +1,9 @@
+function Main() {
+    return (
+        <div className="Main">
+            <h1>Portfolio</h1>
+        </div>
+    )
+}
+
+export default Main;
